00473c7a17011431019e118891bbde99ab490184
Berkus <berkus@gmail.com>
2013-11-01 11:58:01 +0200

    Add missing include.

--- lib/aes_128_ctr.cpp
+++ lib/aes_128_ctr.cpp
@@ -6,6 +6,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
 //
+#include <boost/asio/buffer.hpp>
 #include "aes_128_ctr.h"
 #include "crypto.h"
 
