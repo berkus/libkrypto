c8ee7fff82b966aeeb8e5002514c39537c77c1f7
Berkus <berkus@gmail.com>
2013-12-30 02:15:20 +0200

    Fix test license header.

--- tests/test_aes_128_ctr.cpp
+++ tests/test_aes_128_ctr.cpp
@@ -1,5 +1,11 @@
-/// Copyright (c) 2013, Aldrin's Notebook.
-/// http://opensource.org/licenses/BSD-2-Clause
+//
+// Part of Metta OS. Check http://metta.exquance.com for latest version.
+//
+// Copyright 2007 - 2013, Stanislav Karchebnyy <berkus@exquance.com>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
+//
 #define BOOST_TEST_MODULE Test_aes_128_ctr
 #include <boost/test/unit_test.hpp>
 
